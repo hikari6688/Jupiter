55e87d143d059c291835bb9dd8c0869609c300ba
hikari6688 <hpcgg@foxmail.com>
2021-06-22 17:31:00 +0800

    update request@hook

--- src/hooks/request.ts
+++ src/hooks/request.ts
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import {
+import React, { useEffect, useState, useRef } from 'react';
+import axios, {
   CancelToken,
   AxiosResponse,
   AxiosRequestConfig,
-  AxiosInstance,
   AxiosError,
 } from 'axios';
 import request from '../util/request';
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
 interface ReuqestArgs extends AxiosRequestConfig {
   manual: boolean;
 }
-function useRequest({ url, method, data, manual }: ReuqestArgs) {
+function useRequest<T>(config: ReuqestArgs) {
+  const configRef = useRef<ReuqestArgs>(config);
+  const { manual, data, url, method } = configRef.current;
   const [loading, setLoading] = useState<boolean>(false);
   const [err, setErr] = useState<AxiosError>(null);
-  const [state, setState] = useState<any>(null);
+  const [state, setState] = useState<T>(null);
   useEffect(() => {
     !manual && run();
   }, [data]);
@@ -24,6 +27,7 @@ function useRequest({ url, method, data, manual }: ReuqestArgs) {
         method,
         url,
         data,
+        cancelToken: source.token,
       });
       setState(response.data);
       return Promise.resolve(response);
@@ -34,6 +38,10 @@ function useRequest({ url, method, data, manual }: ReuqestArgs) {
       setLoading(false);
     }
   };
-  return { loading, state, err, run };
+  const cancel = (message: any) => {
+    source.cancel(message);
+    setLoading(false);
+  };
+  return { loading, state, err, run, cancel };
 }
 export default useRequest;
