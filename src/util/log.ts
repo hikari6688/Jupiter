6e96b4913eed901aec7a466d33cfd4d029ddedaf
hikari6688 <hpcgg@foxmail.com>
2021-06-22 18:12:17 +0800

    修改日志输出样式

--- src/util/log.ts
+++ src/util/log.ts
@@ -4,15 +4,15 @@ export const print = (message) => {
   }
   console.log(
     '%c' + '日志' + '%c' + message,
-    'color:#fff;background:#5cb87a;padding:2px 4px;border-radius:2px 0 0 2px',
-    'color:white;background:#409eff;padding:2px 4px;border-radius:0 2px 2px 0'
+    'color:#fff;background:#5cb87a;padding-right:4px;padding-left:4px;padding-top:2px;padding-bottom:2px;border-radius:4px 0 0 4px;font-size:12px;',
+    'color:white;background:#409eff;padding-right:6px;padding-left:6px;padding-top:2px;padding-bottom:2px;border-radius:0 4px 4px 0'
   );
 };
 
 export function debug(err) {
   console.log(
     '%c错误' + '%c' + err,
-    'color:#fff;background:#f56c6c;padding:2px 4px;border-radius:2px 0 0 2px',
-    'color:#fff;background:#fadb14;padding:2px 4px;border-radius:0 2px 2px 0'
+    'color:#fff;background:#f56c6c;padding-right:4px;padding-left:4px;padding-top:2px;padding-bottom:2px;border-radius:2px 0 0 2px',
+    'color:#fff;background:#fadb14;padding-right:6px;padding-left:6px;padding-top:2px;padding-bottom:2px;border-radius:0 2px 2px 0'
   );
 }
